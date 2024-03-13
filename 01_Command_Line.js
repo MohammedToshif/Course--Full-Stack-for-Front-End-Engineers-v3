a57b016f397582a822e413acecef120caa1ab236
MohammedToshif <toshif92612@gmail.com>
2024-03-13 17:03:11 +0530

    command line

---
+++ 01_Command_Line.js
@@ -0,0 +1,85 @@
+
+//----- (01) - (Command Line Exercise)
+
+
+// 1.) === (Diving in)
+    // Terminals and command Lines
+    // VIM
+    // Shells
+
+
+// Terminal nad command Lines :-
+
+// ls => list directory contents
+// cd => change directory
+// mkdir => make directory
+// rmdir => remove directory
+
+// cat => show file content
+// man => command manual
+// less => show file content by page
+// touch => create an empty file
+// rm => remove file
+// echo => repeat input
+
+// Exercise :-
+
+// Navigate to your home directory
+// Make a directory called "temp"
+// Move into temp
+// List the directory contents
+// Make a file called "hello"
+// List the directory contents
+// Move out of temp
+// Delete temp
+
+
+
+//----- (02) - (Command Line Solutions)
+
+
+// ~ cd ~               // (home directory)
+
+// ~ pwd         // (print working directory) (=> /home/toshif) 
+
+// ~ cd /home/toshif
+
+// ~ cd ~
+
+// ~ mkdir temp
+
+// ~ ls
+
+
+// ~ cd ~ temp
+
+// temp ls      // (temp: command not found)
+
+// temp touch hello      // (temp: command not found) 
+
+// temp ls
+
+// temp ls -la
+
+// temp cd ..
+
+// cd../../../../       // (only one move)
+
+// ls
+
+// rmdir temp
+
+// rm temp
+
+// rm -rf 
+
+// man rm
+
+// man rm
+
+// man rm
+
+
+
+//----- (03) - (VIM Exercise)
+
