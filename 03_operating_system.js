c165bde0a12e5d4a47d3590a94b53d8258d09d19
MohammedToshif <toshif92612@gmail.com>
2024-03-15 17:14:03 +0530

    operating system

---
+++ 03_operating_system.js
@@ -0,0 +1,51 @@
+
+//----- (01) - (Operating System)
+
+// Operating System
+
+// OS (defination) => An operating system (OS) is the program that, after being initially loaded into the computer by a boot program, manages all of the other application programs in a computer.
+
+
+/*
+1.) User
+2.) Shell
+3.) Kernel
+----------------
+| 4.) hardware | 
+----------------
+
+
+//----- (02) - (Security & Hashing)
+
+// Security
+
+/*
+1.) username / password
+2.) biometric
+3.) ssh key 
+
+ssh key => SSH keys are authentication credentials. SSH (Secure Shell) is used for managing networks, operating systems, and configurations. It is also inside many file transfer tools and configuration management tools. Every major corporation uses it, in every data center.    */
+
+// Dictionary attack
+
+/*
+123456, 123456789, qwerty, password, 12345, qwerty123, 1q2w3e, 12345678,
+111111, 1234567890
+X => p@ssword12
+p_#s2!$DR   (right)     */
+
+
+// Hashing => Hashing is the process of transforming any given key or a string of characters into another value.
+
+// OpenSSL => is an open source cryptographic toolkit that facilitates secure communications between endpoints on a network. 
+
+
+// terminal
+/*
+temp echo password >> foo
+temp ls
+temp cat foo        // password
+temp man openssl
+
+temp openssl md5 foo | awk
+temp openssl md5 foo    // MD5(foo)= 286755fad04869ca523320acce0dc6a4
