daf9eb2113c898ac704ebe8590313ff4fba9abd2
MohammedToshif <toshif92612@gmail.com>
2024-03-16 17:02:30 +0530

    The Internet

---
+++ 04_the_internet.js
@@ -0,0 +1,34 @@
+
+//----- (01) - (How the internet works)
+
+// understading the internet
+
+/*
+    How the intenet works
+    Buying a domain
+    putting it together
+*/
+
+// How requests works :-
+
+// computer --> network card --> router --> ISP (internet service provider) --> Tier 1 ISP --> Tier 1 ISP --> Datacenter --> server cluster --> load balancer --> server
+
+
+// Terminology :-
+
+/*
+    Internet = a network of networks
+    Intranet = private network
+    LAN = local area network
+    WAN = wide are network
+
+    IP = internet protocol
+    IP Address = numeric lable assigned to an internet connected device
+
+    IPv4 = 8.8.8.8
+    IPv6 = 2001:4860:4860:8888
+*/    
+
+
+
+//----- (02) - (Networks Tools Exercise)
