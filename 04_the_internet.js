436570826376590f71f2c665649a1f063dc54b25
MohammedToshif <toshif92612@gmail.com>
2024-03-19 17:06:47 +0530

    the internet

--- 04_the_internet.js
+++ 04_the_internet.js
@@ -164,3 +164,73 @@ query parameter => test=true
     =>  @
     =>  www
 */    
+
+
+
+//----- (06) - (Update & Restart Server)
+
+// server setup
+
+/*
+    1.) Server setup
+    2.) Web servers
+    3.) NodeJS
+*/
+
+// Brand new server
+
+/*
+    1.) update software
+    2.) Restart your server
+    3.) create a new user
+    4.) make that user a *superuser*
+    5.) enable login for new user
+    6.) disable root login
+*/
+
+
+// updating and resatrting
+
+/*
+    1.) ssh into your server
+    2.) update software
+    // apt update
+    // apt upgrade
+
+    3.) Restart your server
+    // shutdown now -r      // (-r = restart)
+*/    
+
+
+
+//----- (07) - (Create a User)
+
+// sudo
+
+/*
+    #
+
+    root - highest permission level.
+    Allows unrestricted access to
+    the OS
+
+    sudo - super user do. Allows you
+    to run commands and programs as
+    root
+*/    
+
+// creating and updating users
+
+/*
+    1.) create a new user
+    // adduser <your_username>
+
+    2.) add user to "sudo" group
+    // usermod -aG sudo <your_username>
+
+    3.) switch user
+    // su <your_username>
+
+    4.) check sudo access
+    // sudo cat /var/log/auth.log
+*/    
