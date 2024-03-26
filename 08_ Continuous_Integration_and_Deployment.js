cfb2d05faa8a4bf4ccf283fcc1a5979f4bf936d7
MohammedToshif <toshif92612@gmail.com>
2024-03-26 18:18:10 +0530

    Continuous Integration & Deployment

--- 08_ Continuous_Integration_and_Deployment.js
+++ 08_ Continuous_Integration_and_Deployment.js
@@ -44,3 +44,50 @@
     NETFLIX runs on Spinnaker.
     its for create personal project.
 */                                
+
+
+
+//----- (02) - (Cron for CI)
+
+
+// cron - executes commands on  a schedule
+
+// cron
+/*
+    0 10 * * 1-5 sh ./myscript.sh
+
+    "Run ./myscript.sh at 10:00 on every day-of-week fro throught friday."
+*/
+
+// shell scripting
+/*
+    #! /user/bin/bash
+
+    now=$(date + "%r")
+    read -p "what is your name? " name
+    echo "The time is $now. Have a wonderful day $name"
+
+    Wha is you name? toshif
+    The time is 05:30:09 AM. Have a wonderful day toshif.
+*/    
+
+
+// CI/CD
+
+/*
+    1.) Create a bash script that git pulls from main branch
+    2.) create a cron job to execute script every 2 minutes
+
+
+    #! <location of bash>
+    <commands to run>
+
+    1.) Shows location of bash shell
+    // which bash
+
+
+    // terminal
+    cd /var/www/app
+    vi test.sh
+    which bash      /usr/bin/bash
+    vi test.sh
