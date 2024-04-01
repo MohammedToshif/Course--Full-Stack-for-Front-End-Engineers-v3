3222abf19829461085ef0a5edb41f0843b0af8f5
MohammedToshif <toshif92612@gmail.com>
2024-04-01 18:33:49 +0530

    realtime and database

--- 09_Realtime_and_Databases.js
+++ 09_Realtime_and_Databases.js
@@ -39,3 +39,86 @@ Exercise : websocket
         server.listen(3000,function (){console.log("listening on 3000")})
 
     */   
+
+
+
+
+        //----- (04) - (Databases Overview)
+
+        // Databases
+        
+        /*
+        // Relational
+
+        1.) SQL
+        2.) Tables
+        3.) Related data
+        4.) Strict structure
+
+        // relational database very structured, very strict,
+        they have strong opinion about how and where you write and
+        retrieve data.
+
+        // non relational database
+
+        1.) NoSQL
+        2.) Data agnostic
+        3.) Loose structure
+        */        
+
+        // databases
+        /*
+            users
+
+            userID            name              location
+                            (string)            (string)
+
+                1            jem                california                            
+                2            Marc               Minnesota
+
+        name & location = fields
+        1 & 2 = records                
+
+        */
+
+
+        /*
+         // users = userID, name, location, email
+
+        //  pets = petID, userID, name, type
+        // food = foodID, userID, name, type, spicy
+
+        // pets & food  connect with users
+
+        SQL - structured query language
+
+        SELECT * FROM users WHERE name = jem
+
+        SELECT food.type, users.name
+        FROM food
+        LEFT JOIN users ON users.userID = food.userID
+        Where food.name = ramen
+        */
+
+
+
+        //----- (05) - (SQLite)
+
+        // SQLite
+        /*
+            1.) install sqlite3
+            // npm install sqlite3
+
+            2.) set up a database
+             a.) create a atable named "visitors"
+             b.) create a column named "count"
+             c.) create a column named "time"
+
+             3.) on connection, save current visitors count
+
+            //  some work in index_ws.js file (begin database)
+        */
+       
+             
+
+        //----- (06) - (HTTPS Overview)
