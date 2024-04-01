b98781f38e866c6d98f28ab48e819da2d51613df
MohammedToshif <toshif92612@gmail.com>
2024-04-01 14:42:44 +0530

    Realtime and database

--- 09_Realtime_and_Database/index_ws.js
+++ 09_Realtime_and_Database/index_ws.js
@@ -9,3 +9,36 @@ const express = require('express');
 
     server.on('request', app);
     server.listen(8080, function () { console.log('Listening on 8080'); });
+
+
+    //----- (02) - (Using Websockets with Express)
+    
+    //----- Begin websocket 
+
+    const WebSocketServer = require('ws').server;
+
+    const wss = new WebSocketServer({server: server});
+
+    wss.on('connection', function connection(ws) {
+
+        const numClients = wss.clients.size;
+        console.log('Clients connected', numClients);
+
+        wss.broadcast(`Current visitors: ${numClients}`);
+
+        if (ws.readyState === ws.OPEN) {
+            ws.send('Welcome to my server');
+        }
+
+        ws.on('close', function close() {
+        wss.broadcast(`Current visitors: ${numClients}`);
+            console.log('A client has disconnected');
+        });
+
+    });
+
+    wss.broadcast = function broadcast(data) {
+        wss.clients.forEach(function each(client) {
+            client.send(data);
+        })
+    }
